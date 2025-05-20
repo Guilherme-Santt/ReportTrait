808c910e8e0fe3a46334d97768753976e3715f70
José Guilherme <guisant@icloud.com>
2025-05-20 14:12:49 -0300

    Create table

--- main/script.js
+++ main/script.js
@@ -1,5 +1,36 @@
+function criarTabelaVazia() {
+    const tabela = document.createElement('table');
+    const cabecalho = tabela.createTHead();
+    const linhaCabecalho = cabecalho.insertRow();
+
+    const th1 = document.createElement('th');
+    th1.textContent = 'Nome';
+    linhaCabecalho.appendChild(th1);
+
+    const th2 = document.createElement('th');
+    th2.textContent = 'Idade';
+    linhaCabecalho.appendChild(th2);
+
+    const th3 = document.createElement('th');
+    th3.textContent = 'Hobbie';
+    linhaCabecalho.appendChild(th3);
+
+    // Cria o corpo da tabela
+    const corpoTabela = tabela.createTBody();
+    
+    for (let i = 0; i < 3; i++) {
+        const linha = corpoTabela.insertRow();
+        const celula1 = linha.insertCell();
+        const celula2 = linha.insertCell();
+        const celula3 = linha.insertCell();
+    }
+    
+    return tabela;
+}
+
+
 async function pegaDados(){
-    let dados   = await fetch('http://127.0.0.1:5500/main/response.js')    
+    let dados   = await fetch('http://127.0.0.1:5500/main/response.js')
     const data  = await dados.json();
     
     let nome = [];
@@ -18,7 +49,6 @@ async function pegaDados(){
         }
     })
 
-    console.log(nome)
     // Contando dados de acordo com sexo
     let contagemF = 0;
     let contagemM = 0;
@@ -29,7 +59,17 @@ async function pegaDados(){
             contagemM++;
         }
     })
+    const total = contagemF + contagemM;
+    let spanT = document.querySelector('#spanT');
+    let M = document.querySelector('#M');
+    let F = document.querySelector('#F');
+    spanT.innerHTML = total;
+    M.innerHTML = contagemM;
+    F.innerHTML = contagemF;
 
-    console.log(contagemF)
+
+    const response = document.querySelector('#response')
+    const tabelaVazia = criarTabelaVazia();
+    response.appendChild(tabelaVazia);
 }
 pegaDados()
